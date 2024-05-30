e0b492ad61578ae743c30ed4453f5b86589e3298
David Villeda <42588903+DavidITT@users.noreply.github.com>
2024-05-30 13:41:40 -0600

    fix: Changing favicons

--- app.vue
+++ app.vue
@@ -5,7 +5,18 @@
 </template>
 
 <script setup>
+import { useHead } from '#imports'
 import { initFlowbite } from 'flowbite'
+
+useHead({
+  titleTemplate: 'David Villeda',
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: '/favicons/favicon.ico' },
+    { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicons/apple-touch-icon.png' },
+    { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicons/favicon-32x32.png' },
+    { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicons/favicon-16x16.png' }
+  ]
+})
 
 onMounted(() => {
   initFlowbite();
--- layouts/default.vue
+++ layouts/default.vue
@@ -4,24 +4,6 @@
   </main>
 </template>
 
-<script setup>
-
-useHead({
-  titleTemplate: 'David Villeda',
-  app: {
-    head: {
-      link: [
-        {rel: 'icon', type: 'image/x-icon', href: 'assets/favicons/favicon.ico'},
-        {rel: 'apple-touch-icon', sizes: '180x180', href: 'assets/favicons/apple-touch-icon.png'},
-        {rel: 'icon', type: 'image/png', sizes: '32x32', href: 'assets/favicons/favicon-32x32.png'},
-        {rel: 'icon', type: 'image/png', sizes: '16x16', href: 'assets/favicons/favicon-16x16.png'}
-      ]
-    }
-  }
-})
-
-</script>
-
 <style>
 
 body {
