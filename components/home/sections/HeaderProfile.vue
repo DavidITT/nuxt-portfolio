c6a82761f9c5563aedfd4cc7fbf7531e0266a6de
David Villeda <42588903+DavidITT@users.noreply.github.com>
2024-06-06 16:52:33 -0600

    fix: error with links (names)

--- components/home/sections/HeaderProfile.vue
+++ components/home/sections/HeaderProfile.vue
@@ -1,5 +1,6 @@
 <template>
-  <header class="text-center lg:text-left lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24 ">
+  <header
+      class="text-center lg:text-left lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24 ">
     <div>
       <h1 class="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl text-[#2D465C] dark:text-white"
           id="title">
@@ -16,35 +17,58 @@
       <ProfilePhoto class="block lg:hidden mb-10"/>
       <nav class="nav hidden lg:block" aria-label="In-page jump links">
         <ul class="mt-16 w-max">
-<!--          <li v-for="link in navLinks" :key="link.id">-->
-<!--            <a :class="['group flex items-center py-3', { 'active': link.id === activeLink }]" :href="link.href" :target="link.target || '_self'" @click="setActiveLink(link.id)">-->
-<!--              <span :class="['nav-indicator mr-4 h-1 w-5 transition-transform duration-300 transform', { 'rotate-90 bg-emerald-500': link.id === activeLink, 'group-hover:rotate-90 group-hover:bg-emerald-500 group-focus-visible:rotate-45 group-focus-visible:bg-teal-400': true }]"></span>-->
-<!--              <span :class="['nav-text text-xs font-bold uppercase tracking-widest', { 'text-emerald-500': link.id === activeLink, 'text-[#9B51E0] group-hover:text-[#01B47C] group-focus-visible:text-[#01B47C]': true }]">-->
-<!--                {{ link.text }}-->
-<!--              </span>-->
-<!--            </a>-->
-<!--          </li>-->
-          <li v-for="link in navLinks" :key="link.id">
-            <a
-                :class="['group flex items-center py-3', { 'active': link.id === activeLink }]"
-                :href="link.href"
-                :target="link.target || '_self'"
-                @click="handleClick(link.id, $event, link.target)"
-            >
-          <span
-              :class="[
-              'nav-indicator mr-4 h-1 w-5 transition-transform duration-300 transform',
-              { 'rotate-90 bg-emerald-500': link.id === activeLink, 'group-hover:rotate-90 group-hover:bg-emerald-500 group-focus-visible:rotate-45 group-focus-visible:bg-teal-400': true }
-            ]"
-          ></span>
-              <span
-                  :class="[
-              'nav-text text-xs font-bold uppercase tracking-widest',
-              { 'text-emerald-500': link.id === activeLink && link.id !== 'cv', 'text-[#9B51E0] group-hover:text-[#01B47C] group-focus-visible:text-[#01B47C]': true }
-            ]"
-              >
-            {{ link.text }}
-          </span>
+          <li>
+            <a :class="['group flex items-center py-3', { 'active': $t('links.about.name') === activeLink }]"
+               @click="handleClick($t('links.about.name'), $event)">
+                      <span :class="['nav-indicator mr-4 h-1 w-5 transition-transform duration-300 transform', { 'rotate-90 bg-emerald-500': $t('links.about.name') === activeLink, 'group-hover:rotate-90 group-hover:bg-emerald-500 group-focus-visible:rotate-45 group-focus-visible:bg-teal-400': true }]"></span>
+                      <span :class="['nav-text text-xs font-bold uppercase tracking-widest',{ 'text-emerald-500': $t('links.about.name') === activeLink, 'text-[#9B51E0] group-hover:text-[#01B47C] group-focus-visible:text-[#01B47C]': true }]">
+                          {{ $t('links.about.text') }}
+                      </span>
+            </a>
+          </li>
+          <li>
+            <a :class="['group flex items-center py-3', { 'active': $t('links.experience.name') === activeLink }]"
+               @click="handleClick($t('links.experience.name'), $event)">
+              <span :class="['nav-indicator mr-4 h-1 w-5 transition-transform duration-300 transform', { 'rotate-90 bg-emerald-500': $t('links.experience.name') === activeLink, 'group-hover:rotate-90 group-hover:bg-emerald-500 group-focus-visible:rotate-45 group-focus-visible:bg-teal-400': true }]"></span>
+              <span :class="['nav-text text-xs font-bold uppercase tracking-widest',{ 'text-emerald-500': $t('links.experience.name') === activeLink, 'text-[#9B51E0] group-hover:text-[#01B47C] group-focus-visible:text-[#01B47C]': true }]">
+                          {{ $t('links.experience.text') }}
+                      </span>
+            </a>
+          </li>
+          <li>
+            <a :class="['group flex items-center py-3', { 'active': $t('links.projects.name') === activeLink }]"
+               @click="handleClick($t('links.projects.name'), $event)">
+              <span :class="['nav-indicator mr-4 h-1 w-5 transition-transform duration-300 transform', { 'rotate-90 bg-emerald-500': $t('links.projects.name') === activeLink, 'group-hover:rotate-90 group-hover:bg-emerald-500 group-focus-visible:rotate-45 group-focus-visible:bg-teal-400': true }]"></span>
+              <span :class="['nav-text text-xs font-bold uppercase tracking-widest',{ 'text-emerald-500': $t('links.projects.name') === activeLink, 'text-[#9B51E0] group-hover:text-[#01B47C] group-focus-visible:text-[#01B47C]': true }]">
+                          {{ $t('links.projects.text') }}
+                      </span>
+            </a>
+          </li>
+          <li>
+            <a :class="['group flex items-center py-3', { 'active': $t('links.technologies.name') === activeLink }]"
+               @click="handleClick($t('links.technologies.name'), $event)">
+              <span :class="['nav-indicator mr-4 h-1 w-5 transition-transform duration-300 transform', { 'rotate-90 bg-emerald-500': $t('links.technologies.name') === activeLink, 'group-hover:rotate-90 group-hover:bg-emerald-500 group-focus-visible:rotate-45 group-focus-visible:bg-teal-400': true }]"></span>
+              <span :class="['nav-text text-xs font-bold uppercase tracking-widest',{ 'text-emerald-500': $t('links.technologies.name') === activeLink, 'text-[#9B51E0] group-hover:text-[#01B47C] group-focus-visible:text-[#01B47C]': true }]">
+                          {{ $t('links.technologies.text') }}
+                      </span>
+            </a>
+          </li>
+          <li>
+            <a :class="['group flex items-center py-3', { 'active': $t('links.certificates.name') === activeLink }]"
+               @click="handleClick($t('links.certificates.name'), $event)">
+              <span :class="['nav-indicator mr-4 h-1 w-5 transition-transform duration-300 transform', { 'rotate-90 bg-emerald-500': $t('links.certificates.name') === activeLink, 'group-hover:rotate-90 group-hover:bg-emerald-500 group-focus-visible:rotate-45 group-focus-visible:bg-teal-400': true }]"></span>
+              <span :class="['nav-text text-xs font-bold uppercase tracking-widest',{ 'text-emerald-500': $t('links.certificates.name') === activeLink, 'text-[#9B51E0] group-hover:text-[#01B47C] group-focus-visible:text-[#01B47C]': true }]">
+                          {{ $t('links.certificates.text') }}
+                      </span>
+            </a>
+          </li>
+          <li>
+            <a :class="['group flex items-center py-3', { 'active': $t('links.cv.name') === activeLink }]"
+               :href="locale === 'en' ? '/David_Villeda_CV_EN.pdf' : '/David_Villeda_CV_ES.pdf'">
+              <span :class="['nav-indicator mr-4 h-1 w-5 transition-transform duration-300 transform', { 'rotate-90 bg-emerald-500': $t('links.cv.name') === activeLink, 'group-hover:rotate-90 group-hover:bg-emerald-500 group-focus-visible:rotate-45 group-focus-visible:bg-teal-400': true }]"></span>
+              <span :class="['nav-text text-xs font-bold uppercase tracking-widest',{ 'text-emerald-500': $t('links.cv.name') === activeLink, 'text-[#9B51E0] group-hover:text-[#01B47C] group-focus-visible:text-[#01B47C]': true }]">
+                          {{ $t('links.cv.text') }}
+                      </span>
             </a>
           </li>
         </ul>
@@ -57,14 +81,13 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import { computed } from 'vue';
-import { defineEmits } from 'vue';
-import { useI18n } from 'vue-i18n';
+import {ref} from 'vue';
+import {computed} from 'vue';
+import {useI18n} from 'vue-i18n';
 import SocialLinksAndCV from "../SocialLinksAndCV.vue";
 import ProfilePhoto from "../ProfilePhoto.vue";
 
-const { locale, messages } = useI18n();
+const {locale, messages} = useI18n();
 const activeLink = ref('about');
 const emit = defineEmits(['update:activeSection']);
 
@@ -91,12 +114,7 @@ const setActiveLink = (linkId) => {
   activeLink.value = linkId;
 }
 
-function handleClick(linkId, event, target) {
-
-  if (target === '_blank') {
-    activeLink.value = 'about';
-    return;
-  }
+function handleClick(linkId, event) {
   event.preventDefault();
   activeLink.value = linkId;
   emit('update:activeSection', linkId);
