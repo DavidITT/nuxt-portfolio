28a1bc99a1b055ed3448a59dd96596b3320b4807
David Villeda <42588903+DavidITT@users.noreply.github.com>
2024-06-10 16:55:09 -0600

    feat: show certificates with links

--- pages/home.vue
+++ pages/home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
+  <div class="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-10 lg:py-0">
     <div class="lg:flex lg:justify-between lg:gap-4">
       <HeaderProfile  @update:activeSection="updateActiveSection"/>
       <main id="content" class="pt-24 lg:w-1/2 lg:py-24">
