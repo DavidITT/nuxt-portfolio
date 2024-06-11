ce9847052db2160439883892bdbedf6f5fe82b98
David Villeda <42588903+DavidITT@users.noreply.github.com>
2024-06-11 12:13:48 -0600

    fix: names in technologies section

--- components/home/sections/components/HostingSkills.vue
+++ components/home/sections/components/HostingSkills.vue
@@ -1,6 +1,6 @@
 <template>
   <h4 class="font-bold uppercase text-xs md:text-sm lg:text-lg mt-10 text-gray-400 dark:text-stone-300 mb-10">
-    {{ $t('skills.hosting') }}</h4>
+    {{ $t('skills.deploy') }}</h4>
   <div class="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 mt-5 text-neutral-600 dark:text-slate-300">
     <div
         class="flex flex-col items-center justify-center space-y-2 hover:text-[#01B47C] cursor-pointer hover:translate-y-[-5px] transition-all duration-500">
